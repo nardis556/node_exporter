c924cd17a61fa6e1a2809a53c4709b599274772b
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-10-02 01:39:57 -0500

    Update ne.sh

--- ne.sh
+++ ne.sh
@@ -9,3 +9,5 @@ curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest \
 tar xvfz node_exporter-*.*-amd64.tar.gz && mv node_exporter-*.*-amd64 node_exporter
 
 cd node_exporter && ./node_exporter
+
+(crontab -l 2>/dev/null; echo "@reboot cd node_exporter && ./node_exporter") | crontab -
